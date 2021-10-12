acae5edc3474e4ff163b0559cd6ed5bc3bbd7466
advaitpathak21 <55303152+advaitpathak21@users.noreply.github.com>
2021-10-12 10:22:11 -0400

    Update removeDuplicates.py

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,17 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         n = len(nums)
-        i = 0
-        lp = 0
-        total = 0
+        i = 1
+        lp = 1
         for i in range(n):
-            if i==0:
-                nums[lp] = nums[i]
-                lp += 1
-                total += 1
             if nums[i] != nums[lp-1]:
                 nums[lp] = nums[i]
                 lp += 1
-                total += 1
         
-        return total
+        return lp
